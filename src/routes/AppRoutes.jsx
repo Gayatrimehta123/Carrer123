b24cc32d20b1af75b391ab62b821bb9e7c9e1664
Ravindra Kumar <kumarravindra54787@gmail.com>
2025-09-17 23:31:37 +0530

    Fixed AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,12 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 
-function AppRoutes() {
-  return (
-    <Router>
-      <Routes>
-      </Routes>
-    </Router>
-  );
-}
-
-export default AppRoutes;
+export const router = createBrowserRouter([
+ 
+]);
