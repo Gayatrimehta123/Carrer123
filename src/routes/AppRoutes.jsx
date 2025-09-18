64c5985b5ca94cd3d7d901f7172edf54c78b7324
Ravindra Kumar <kumarravindra54787@gmail.com>
2025-09-18 20:56:51 +0530

    Added route examples

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
- 
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+            },
+
+        ],
+    },
+    {
+        path: "/auth/login",
+        element: <LoginPage />
+
+    }
 ]);
