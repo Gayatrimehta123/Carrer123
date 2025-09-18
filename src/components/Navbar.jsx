5b075e6f66f3f80796db043dcf01230422e3a1df
Ravindra Kumar <kumarravindra54787@gmail.com>
2025-09-18 21:34:52 +0530

    Added new files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+function navbar() {
+  return (
+    <nav>
+      <h1>My Website</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default navbar;
